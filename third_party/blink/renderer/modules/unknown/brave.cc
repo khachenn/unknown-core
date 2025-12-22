b951eac76378df5b7a55a0db9955433ae8f1502a
Khachen Noisriphum <khachen.dev@gmail.com>
2025-12-22 16:12:18 +0700

    refactor: update references from 'brave' to 'unknown' in various files

--- third_party/blink/renderer/modules/unknown/brave.cc
+++ third_party/blink/renderer/modules/unknown/brave.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "unknown/third_party/blink/renderer/modules/brave/brave.h"
+#include "unknown/third_party/blink/renderer/modules/unknown/brave.h"
 
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
